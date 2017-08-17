3cadc2013469fb5ec7f2fd0a2b3136691ee97fc9
xiaodi <xiaodi@jlcxtx.com>
2017-08-17 15:22:07 +0800

    添加项目文件。

---
+++ NFine.Site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NFine.Site.MvcApplication" Language="C#" %>
---
+++ NFine.Site/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NFine.Site.Startup))]
+namespace NFine.Site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
